9e37a73d7154144cd48e15776939acc0fde5632c
nguyen3 <nguyen33@mail.usf.edu>
2019-03-19 06:13:17 -0400

    Version 2 of Deliverable 2 - Iterative Statement

--- Deliverable 2_Iterative Statement/Program.cs
+++ Deliverable 2_Iterative Statement/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ Author: Trang Nguyen
+ Date: 1/17/2019
+ Comment: This C# Console application demonstrates 
+          the use of iterative statement from 1 to 100
+*/
+
+using System;
 
 namespace Deliverable_2_Iterative_Statement
 {
@@ -6,7 +13,35 @@ namespace Deliverable_2_Iterative_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            //Ask the user for input
+            Console.WriteLine("Enter an Integer value between 1 and 100 to execute an iterative statement: ");
+
+            //Use try-catch to validate the user input
+            try
+            {
+                //Gather the input
+                string input = Console.ReadLine();
+
+                //Assign input as an integer
+                int value_of_input = int.Parse(input);
+
+                //Execute For Loop
+                for (int i = 0; i < value_of_input; i++)
+                {
+                    Console.WriteLine("You have entered " + value_of_input.ToString() + ". This is the current integer value in the loop: " + i.ToString());
+                }
+                Console.WriteLine("Press any key to exit the program...");
+                //Read key to exit the program
+                Console.ReadKey(true); 
+            }//End of try
+
+            catch
+            {
+                Console.WriteLine("Please enter an integer value and try running the program again...");
+                Console.WriteLine("Press any key to exit the program...");
+                //Read key to exit the program
+                Console.ReadKey(true);
+            }//End of catch
+        }//End of Main
+    }//End of class
+}//End of namespace
